f5bed7e74d8a27401dfe1573053964c2a2fbdc5c
Matt Brown <github@muglug.com>
2026-06-17 08:39:04 -0400

    Fix 2 ok tests

--- tests/inference/ArrayAssignment/ignoreInvalidArrayOffset/input.php
+++ tests/inference/ArrayAssignment/ignoreInvalidArrayOffset/input.php
@@ -8,7 +8,6 @@ $a["b"]["c"] = 0;
 foreach ([1, 2, 3] as $i) {
     /**
      * @psalm-suppress InvalidArrayOffset
-     * @psalm-suppress PossiblyUndefinedArrayOffset
      */
     $a["b"]["d"] += $a["b"][$i];
 }
